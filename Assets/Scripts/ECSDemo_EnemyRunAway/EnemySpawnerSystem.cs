4f280dc166d4870e34dda746407223c4756c18b7
RomainSepulchre <47340483+RomainSepulchre@users.noreply.github.com>
2025-03-21 21:22:20 -0400

    Write enemy spawner system

--- Assets/Scripts/ECSDemo_EnemyRunAway/EnemySpawnerSystem.cs
+++ Assets/Scripts/ECSDemo_EnemyRunAway/EnemySpawnerSystem.cs
@@ -1,9 +1,47 @@
+using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace ECS.EnemyRunAwayDemo
 {
     public partial struct EnemySpawnerSystem : ISystem
     {
-    
+        EntityQuery spawnerQuery;
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            spawnerQuery = SystemAPI.QueryBuilder().WithAll<EnemySpawner>().Build();
+            state.RequireForUpdate(spawnerQuery);
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            foreach (var (spawner, entity) in SystemAPI.Query<RefRO<EnemySpawner>>().WithEntityAccess())
+            {
+                for (var i = 0; i < spawner.ValueRO.SpawnAmount; i++)
+                {
+                    Entity newEnemy = state.EntityManager.Instantiate(spawner.ValueRO.EnemyToSpawn);
+
+                    Random random = Random.CreateFromIndex((uint)((SystemAPI.Time.ElapsedTime / SystemAPI.Time.DeltaTime) + i));
+
+                    // Set a random position in the spawn area
+                    float xPos = random.NextFloat(-spawner.ValueRO.SpawnAreaXLimit, spawner.ValueRO.SpawnAreaXLimit);
+                    float yPos = random.NextFloat(-spawner.ValueRO.SpawnAreaYLimit, spawner.ValueRO.SpawnAreaYLimit);
+                    float3 spawnPosition = new float3(xPos, yPos, 0);
+                    state.EntityManager.SetComponentData(newEnemy, LocalTransform.FromPosition(spawnPosition));
+
+                    // Set Player on enemy
+                    Enemy enemyComp = state.EntityManager.GetComponentData<Enemy>(newEnemy);
+                    enemyComp.Player = spawner.ValueRO.Player;
+                    state.EntityManager.SetComponentData(newEnemy, enemyComp);
+                }
+
+                // Disabled spawner component once everything has been spawned
+                state.EntityManager.SetComponentEnabled(entity, ComponentType.ReadWrite<EnemySpawner>(), false);
+            }
+        }
     } 
 }
--- Assets/Scripts/ECSDemo_EnemyRunAway/PlayerSystem.cs
+++ Assets/Scripts/ECSDemo_EnemyRunAway/PlayerSystem.cs
@@ -11,6 +11,7 @@ namespace ECS.EnemyRunAwayDemo
         SystemHandle inputSysHandle;
         EntityQuery playerQuery;
 
+        // Cannot BurstCompile state.World.GetExistingSystem<InputSystem>()
         public void OnCreate(ref SystemState state)
         {
             inputSysHandle = state.World.GetExistingSystem<InputSystem>();
