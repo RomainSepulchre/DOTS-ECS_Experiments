72f4c8bbb7164770b726f3f78108cad82882450c
RomainSepulchre <47340483+RomainSepulchre@users.noreply.github.com>
2025-04-08 00:37:35 -0400

    Implement a culling job with no branching in the code

--- Assets/Scripts/SIMD_SimpleFustrum/FustrumCullingSystem.cs
+++ Assets/Scripts/SIMD_SimpleFustrum/FustrumCullingSystem.cs
@@ -31,19 +31,32 @@ namespace Burst.SIMD.SimpleFustrum
             _planes.Dispose();
         }
 
+        // TODO: Add a spawner system and test with huge number of sphere entities 
         [BurstCompile]
         public void DoCulling(ref SystemState state)
         {
-            CullJob cullingJob = new CullJob
+            // Simple culling
+            //CullJob cullingJob = new CullJob
+            //{
+            //    Planes = _planes
+            //};
+            //state.Dependency = cullingJob.ScheduleParallel(state.Dependency);
+            //state.Dependency.Complete(); // why do they complete the job immediately in the example ?
+
+            // Culling without branching in the code
+            CullJobNoBranch cullJobNoBranch = new CullJobNoBranch
             {
                 Planes = _planes
             };
-            state.Dependency = cullingJob.ScheduleParallel(state.Dependency);
-            state.Dependency.Complete();
+            state.Dependency = cullJobNoBranch.ScheduleParallel(state.Dependency);
+            state.Dependency.Complete(); // why do they complete the job immediately in the example ?
         }
     }
 
-    [BurstCompile(OptimizeFor = OptimizeFor.Performance)] // TODO: Checks what OptimizeFor.Performance exactly does ?
+    /// <summary>
+    /// Simple version of the culling job.
+    /// </summary>
+    [BurstCompile(OptimizeFor = OptimizeFor.Performance)] // How should the generated burst-compiled code be optimized (https://docs.unity3d.com/Packages/com.unity.burst@1.8/api/Unity.Burst.OptimizeFor.html)
     partial struct CullJob : IJobEntity
     {
         [ReadOnly] public NativeArray<float4> Planes;
@@ -64,7 +77,35 @@ namespace Burst.SIMD.SimpleFustrum
             }
 
             visibility.Value = visible ? 1 : 0;
+
+            // Change material color to show objects out of the culling
             baseColor.Value = visible ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f,0,0,1f);
+        }
+    }
+
+    /// <summary>
+    /// Version of the culling job that remove the break statement which generate branching in the assembly code.
+    /// The job perform more computation but the performance gains from removing the branch allows the code to run faster than the simple version.
+    /// </summary>
+    [BurstCompile(OptimizeFor = OptimizeFor.Performance)]
+    partial struct CullJobNoBranch : IJobEntity
+    {
+        [ReadOnly] public NativeArray<float4> Planes;
+
+        void Execute(ref SphereVisible visibility, ref HDRPMaterialPropertyBaseColor baseColor, in LocalToWorld localToWorld, in SphereRadius radius)
+        {
+            var pos = localToWorld.Position;
+            visibility.Value =
+                (math.dot(Planes[0].xyz, pos) + Planes[0].w + radius.Value > 0) &&
+                (math.dot(Planes[1].xyz, pos) + Planes[1].w + radius.Value > 0) &&
+                (math.dot(Planes[2].xyz, pos) + Planes[2].w + radius.Value > 0) &&
+                (math.dot(Planes[3].xyz, pos) + Planes[3].w + radius.Value > 0) &&
+                (math.dot(Planes[4].xyz, pos) + Planes[4].w + radius.Value > 0) &&
+                (math.dot(Planes[5].xyz, pos) + Planes[5].w + radius.Value > 0)
+                ? 1 : 0;
+
+            // Change material color to show objects out of the culling
+            baseColor.Value = visibility.Value == 1 ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f, 0, 0, 1f);
         }
     }
 }
