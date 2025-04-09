33f54d347a1773aa0cd9a872c03022b1046de1c0
RomainSepulchre <47340483+RomainSepulchre@users.noreply.github.com>
2025-04-09 19:21:29 -0400

    Implement another fustrum culling job using an IJobChunk and packing sphere instead of fustrum planes to have an even better SIMD optimization.

--- Assets/Scripts/SIMD_SimpleFustrum/FustrumCullingSystem.cs
+++ Assets/Scripts/SIMD_SimpleFustrum/FustrumCullingSystem.cs
@@ -1,11 +1,10 @@
-using System;
 using Unity.Burst;
+using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
-using UnityEngine;
 
 namespace Burst.SIMD.SimpleFustrum
 {
@@ -13,12 +12,14 @@ namespace Burst.SIMD.SimpleFustrum
     {
         private NativeArray<float4> _planes;
         private NativeArray<PlanePacket4> _planePackets;
+        private EntityQuery query;
 
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
             _planes = new NativeArray<float4>(6, Allocator.Persistent);
             _planePackets = new NativeArray<PlanePacket4>(2, Allocator.Persistent); // 2 because we need 2 float4 to store 6 elements
+            query = SystemAPI.QueryBuilder().WithAll<LocalToWorld,SphereRadius>().WithAllRW<SphereVisible, HDRPMaterialPropertyBaseColor>().Build();
         }
 
         // OnUpdate can't be Burst Compiled because we access a managed type (Camera) in FustrumCullingHelper.UpdateFustrumPlanes()
@@ -50,6 +51,7 @@ namespace Burst.SIMD.SimpleFustrum
             //state.Dependency = cullingJob.ScheduleParallel(state.Dependency);
             //state.Dependency.Complete(); // why do they complete the job immediately in the example ?
 
+
             // Culling without branching in the code
             //CullJobNoBranch cullJobNoBranch = new CullJobNoBranch
             //{
@@ -58,13 +60,27 @@ namespace Burst.SIMD.SimpleFustrum
             //state.Dependency = cullJobNoBranch.ScheduleParallel(state.Dependency);
             //state.Dependency.Complete(); // why do they complete the job immediately in the example ?
 
+
             // Culling with PlanePackets to allow SIMD instructions
-            CullJobWithPackets cullJobWithPackets = new CullJobWithPackets
+            //CullJobWithPackets cullJobWithPackets = new CullJobWithPackets
+            //{
+            //    PlanePackets = _planePackets
+            //};
+            //state.Dependency = cullJobWithPackets.ScheduleParallel(state.Dependency);
+            //state.Dependency.Complete(); // why do they complete the job immediately in the example ?
+
+
+            // Culling with SpherePackets to have wider batches of data using SIMD instructions
+            CullJobWithSpherePackets cullJobWithSpherePackets = new CullJobWithSpherePackets
             {
-                PlanePackets = _planePackets
+                LocalToWorldTypeHandle = SystemAPI.GetComponentTypeHandle<LocalToWorld>(true),
+                SphereRadiusTypeHandle = SystemAPI.GetComponentTypeHandle<SphereRadius>(true),
+                SphereVisibleTypeHandle = SystemAPI.GetComponentTypeHandle<SphereVisible>(false), // false = is not read only
+                BaseColorTypeHandle = SystemAPI.GetComponentTypeHandle<HDRPMaterialPropertyBaseColor>(false), // false = is not read only
+                FustrumPlanes = _planes
             };
-            state.Dependency = cullJobWithPackets.ScheduleParallel(state.Dependency);
-            state.Dependency.Complete(); // why do they complete the job immediately in the example ?
+            state.Dependency = cullJobWithSpherePackets.ScheduleParallel(query, state.Dependency);
+            state.Dependency.Complete();
         }
     }
 
@@ -76,7 +92,7 @@ namespace Burst.SIMD.SimpleFustrum
     {
         [ReadOnly] public NativeArray<float4> Planes;
 
-        void Execute(ref SphereVisible visibility, ref HDRPMaterialPropertyBaseColor baseColor, in LocalToWorld localToWorld, in SphereRadius radius)
+        public void Execute(ref SphereVisible visibility, ref HDRPMaterialPropertyBaseColor baseColor, in LocalToWorld localToWorld, in SphereRadius radius)
         {
             bool visible = true;
 
@@ -107,7 +123,7 @@ namespace Burst.SIMD.SimpleFustrum
     {
         [ReadOnly] public NativeArray<float4> Planes;
 
-        void Execute(ref SphereVisible visibility, ref HDRPMaterialPropertyBaseColor baseColor, in LocalToWorld localToWorld, in SphereRadius radius)
+        public void Execute(ref SphereVisible visibility, ref HDRPMaterialPropertyBaseColor baseColor, in LocalToWorld localToWorld, in SphereRadius radius)
         {
             var pos = localToWorld.Position;
             visibility.Value =
@@ -133,7 +149,7 @@ namespace Burst.SIMD.SimpleFustrum
     {
         [ReadOnly] public NativeArray<PlanePacket4> PlanePackets;
 
-        void Execute(ref SphereVisible visibility, ref HDRPMaterialPropertyBaseColor baseColor, in LocalToWorld localToWorld, in SphereRadius radius)
+        public void Execute(ref SphereVisible visibility, ref HDRPMaterialPropertyBaseColor baseColor, in LocalToWorld localToWorld, in SphereRadius radius)
         {
             var pos = localToWorld.Position;
             var p0 = PlanePackets[0];
@@ -166,6 +182,89 @@ namespace Burst.SIMD.SimpleFustrum
 
             // Change material color to show objects out of the culling
             baseColor.Value = visibility.Value == 1 ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f, 0, 0, 1f);
+        }
+    }
+
+    /// <summary>
+    /// Version of the culling job where the use of SIMD optimizations is even better: instead of packing frustrum planes in packets of float4, we pack the spheres to have a wider amount of data packed
+    /// </summary>
+    [BurstCompile(OptimizeFor = OptimizeFor.Performance)]
+    partial struct CullJobWithSpherePackets : IJobChunk
+    {
+        [ReadOnly] public ComponentTypeHandle<LocalToWorld> LocalToWorldTypeHandle;
+        [ReadOnly] public ComponentTypeHandle<SphereRadius> SphereRadiusTypeHandle;
+        [ReadOnly] public NativeArray<float4> FustrumPlanes;
+
+        public ComponentTypeHandle<SphereVisible> SphereVisibleTypeHandle;
+        public ComponentTypeHandle<HDRPMaterialPropertyBaseColor> BaseColorTypeHandle;
+
+        public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+        {
+            // Get array of components in the chunk
+            NativeArray<LocalToWorld>.ReadOnly chunkLocalToWorld = chunk.GetNativeArray(ref LocalToWorldTypeHandle).AsReadOnly();
+            NativeArray<float> chunkSphereRadius = chunk.GetNativeArray(ref SphereRadiusTypeHandle).Reinterpret<float>(); // We reinterpret the data as float to make its repacking easier
+            NativeArray<SphereVisible> chunkSphereVisible = chunk.GetNativeArray(ref SphereVisibleTypeHandle);
+            NativeArray<HDRPMaterialPropertyBaseColor> chunkBaseColor = chunk.GetNativeArray(ref  BaseColorTypeHandle);
+
+            float4 plane0 = FustrumPlanes[0];
+            float4 plane1 = FustrumPlanes[1];
+            float4 plane2 = FustrumPlanes[2];
+            float4 plane3 = FustrumPlanes[3]; 
+            float4 plane4 = FustrumPlanes[4];
+            float4 plane5 = FustrumPlanes[5];
+
+            for (int i = 0; chunk.Count - i >= 4; i+=4) // We check every 4 items because we pack them in float4
+            {
+                // Store positions in float4
+                float3 posA = chunkLocalToWorld[i].Position;
+                float3 posB = chunkLocalToWorld[i+1].Position;
+                float3 posC = chunkLocalToWorld[i+2].Position;
+                float3 posD = chunkLocalToWorld[i+3].Position;
+
+                float4 Xs = new float4(posA.x, posB.x, posC.x, posD.x);
+                float4 Ys = new float4(posA.y, posB.y, posC.y, posD.y);
+                float4 Zs = new float4(posA.z, posB.z, posC.z, posD.z);
+
+                // Store radii in a float4
+                float4 Radii = chunkSphereRadius.ReinterpretLoad<float4>(i); // Load the data from the array as a different type
+
+                // Test the 4 spheres with the 6 fustrum planes
+                // We are inside if the result is bigger than 0, so they all plane checks must be true for a sphere to be inside
+                bool4 mask =
+                    plane0.x * Xs + plane0.y * Ys + plane0.z * Zs + plane0.w + Radii > 0.0f & // AND BIT OPERATION
+                    plane1.x * Xs + plane1.y * Ys + plane1.z * Zs + plane1.w + Radii > 0.0f &
+                    plane2.x * Xs + plane2.y * Ys + plane2.z * Zs + plane2.w + Radii > 0.0f &
+                    plane3.x * Xs + plane3.y * Ys + plane3.z * Zs + plane3.w + Radii > 0.0f &
+                    plane4.x * Xs + plane4.y * Ys + plane4.z * Zs + plane4.w + Radii > 0.0f &
+                    plane5.x * Xs + plane5.y * Ys + plane5.z * Zs + plane5.w + Radii > 0.0f;
+
+                // TODO: ? I don't see why we aren't using a bool in SphereVisible and reinterpret here directly the bool4 ? Why is using ints faster here ?
+                chunkSphereVisible.ReinterpretStore(i, new int4(mask)); // Reintepret the data to store it in the array
+
+                chunkBaseColor[i] = new HDRPMaterialPropertyBaseColor { Value = mask.x ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f, 0, 0, 1f) };
+                chunkBaseColor[i+1] = new HDRPMaterialPropertyBaseColor { Value = mask.y ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f, 0, 0, 1f) };
+                chunkBaseColor[i+2] = new HDRPMaterialPropertyBaseColor { Value = mask.z ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f, 0, 0, 1f) };
+                chunkBaseColor[i+3] = new HDRPMaterialPropertyBaseColor { Value = mask.w ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f, 0, 0, 1f) };
+            }
+
+            // If number of entities isn't divisible by 4, process the entity left
+            for (int i = (chunk.Count >> 2) << 2; i < chunk.Count; i++) // To get the start index we count number of full packet with (chunk.Count >> 2) then we get the start index of next packet with (chunk.Count >> 2) << 2.
+            {
+                float3 pos = chunkLocalToWorld[i].Position;
+                float radius = chunkSphereRadius[i];
+
+                int visible =
+                    (math.dot(plane0.xyz, pos) + plane0.w + radius > 0.0f &&
+                    math.dot(plane1.xyz, pos) + plane1.w + radius > 0.0f &&
+                    math.dot(plane2.xyz, pos) + plane2.w + radius > 0.0f &&
+                    math.dot(plane3.xyz, pos) + plane3.w + radius > 0.0f &&
+                    math.dot(plane4.xyz, pos) + plane4.w + radius > 0.0f &&
+                    math.dot(plane5.xyz, pos) + plane5.w + radius > 0.0f) ? 1 : 0;
+
+                chunkSphereVisible[i] = new SphereVisible { Value = visible };
+
+                chunkBaseColor[i] = new HDRPMaterialPropertyBaseColor { Value = visible == 1 ? new float4(0.5f, 0.5f, 0.5f, 1f) : new float4(1f, 0, 0, 1f) };
+            }
         }
     }
 }
