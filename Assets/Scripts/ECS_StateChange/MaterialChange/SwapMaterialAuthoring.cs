c5abc3892fcb6e482702cb4c43b2e6116eec6573
RomainSepulchre <47340483+RomainSepulchre@users.noreply.github.com>
2025-04-18 18:51:20 -0400

    Fix issue with SwapMaterialAuthoring.cs using dynamic transform flag

--- Assets/Scripts/ECS_StateChange/MaterialChange/SwapMaterialAuthoring.cs
+++ Assets/Scripts/ECS_StateChange/MaterialChange/SwapMaterialAuthoring.cs
@@ -16,8 +16,9 @@ namespace ECS.StateChange
     {
         public override void Bake(SwapMaterialAuthoring authoring)
         {
-            // TODO: Why objects moved at weird position (-750, 20, 750) and not having scale change with dynamic ?
-            //Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+            // ? Why objects moved at weird position (-750, 20, 750) and not having scale change with TransformUsageFlags.Dynamic ?
+            // -> It's simply that ObjectSpawnerSystem process this entity since it query every object with a LocalTransform and TransformUsageFlags.Dynamic add a localTransform to this entity
+            //    I could simply fix this by excluding SwapMaterial component from the ObjectSpawnerSystem query but anyway I don't need to move this cube
             Entity entity = GetEntity(TransformUsageFlags.Renderable);
 
             SwapMaterial newSwapMaterial = new SwapMaterial()
